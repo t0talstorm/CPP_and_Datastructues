b7b85aa152801840c16b75233c6060432804fa2d
Aditya Sinha <sinha.aditya.btech2024@sitpune.edu.in>
2025-07-09 02:53:50 -0400

    Experiment-1a
    Experiment 1a printing hello world

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello!"<<endl;
+    cout << "First Program"<<endl;
+    return 0;
+}
+/*
+hello!
+First Program
+
+=== Code Execution Successful ===
+*/
